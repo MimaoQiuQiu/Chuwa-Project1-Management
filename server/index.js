4647c470182559a38e7340e42956fd2c25ff6f1c
shuaixin li <shuaixinli121@gmail.com>
2023-09-27 22:05:49 -0600

    routes for signin, signup, reset password

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
 
+// api for signup, signin, password reset
+app.use("/api/auth", authRoutes);
 
 app.use(express.json());
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,1 +1,11 @@
 // routes for signup, signin, password reset
+const express = require("express");
+const router = express.Router();
+const { signup, signin, resetPassword } = require("../handlers/auth");
+
+// path: /api/auth
+router.post("/signup", signup);
+router.post("/signin", signin);
+router.put("/resetpassword", resetPassword);
+
+module.exports = router;
