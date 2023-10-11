1ed5944b605e7a2cff36d3cd3745d26ef446c4cd
shuaixin li <shuaixinli121@gmail.com>
2023-10-10 19:07:03 -0600

    add api services to frontend

---
+++ client/src/services/user.js
@@ -0,0 +1,46 @@
+import makeApiCall from "./apiCall";
+
+/**
+ * router.get("/cart", getAllProductsInCart);
+ * router.put("/cart/:productId", editProductQuantityInCart);
+ * router.delete("/cart/:productId", deleteProductInCart);
+ * router.post("/cart/checkout", checkout);
+ */
+
+export const getAllProductsInCart = async (userId) => {
+  const res = await makeApiCall({
+    url: `/api/users/${userId}/cart`,
+    method: "GET",
+  });
+  return res;
+};
+
+export const editProductQuantityInCart = async ({
+  userId,
+  productId,
+  newQuantity,
+}) => {
+  const res = await makeApiCall({
+    url: `/api/users/${userId}/cart/${productId}`,
+    method: "PUT",
+    data: { quantity: newQuantity },
+  });
+  return res;
+};
+
+export const deleteProductInCart = async ({ userId, productId }) => {
+  const res = await makeApiCall({
+    url: `/api/users/${userId}/cart/${productId}`,
+    method: "DELETE",
+  });
+  return res;
+};
+
+export const checkout = async ({ userId, charge }) => {
+  const res = await makeApiCall({
+    url: `/api/users/${userId}/cart/checkout`,
+    method: "POST",
+    data: { charge },
+  });
+  return res;
+};
